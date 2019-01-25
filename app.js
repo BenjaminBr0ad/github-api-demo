ac63ff24d27b10cd37ed224c9d445b4530a740d2
Benjamin J. Broad <benjamin.broad@colorado.edu>
2019-01-25 06:23:55 -0700

    Path fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 app.use('/', indexRouter)
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('client', 'build', 'index.html'));
+  res.sendFile('./client/build/index.html');
 });
 
 if (app.get('env') === 'development') {
