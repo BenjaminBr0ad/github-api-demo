ac35a8e814359385de2cb8e27aff3846ee9c697c
Benjamin J. Broad <benjamin.broad@colorado.edu>
2019-01-25 12:01:25 -0700

    CORS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const indexRouter = require('./routes/index')
 const app = express()
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "https://github-api-demo-bbroad-client.herokuapp.com")
   res.header("Access-Control-Allow-Methods", "POST")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
